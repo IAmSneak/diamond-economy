ecfa40eafdbae69090c40124f4ba884747133347
tcfplayz <chau123ka89chun10p@gmail.com>
2022-01-27 12:06:39 +0800

    update to jave 17
    1.18

---
+++ remappedSrc/com/gmail/sneakdevs/diamondeconomy/mixin/PlayerManagerMixin.java
@@ -0,0 +1,22 @@
+package com.gmail.sneakdevs.diamondeconomy.mixin;
+
+import com.gmail.sneakdevs.diamondeconomy.DatabaseManager;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerManager.class)
+public class PlayerManagerMixin {
+    @Inject(method = "onPlayerConnect", at = @At("TAIL"))
+    private void onPlayerConnectMixin(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
+        DatabaseManager dm = new DatabaseManager();
+        String uuid = player.getUuidAsString();
+        String name = player.getName().asString();
+        dm.addPlayer(uuid, name);
+        dm.setName(uuid, name);
+    }
+}
