6b8cbb31af0c7c21c684f69a40683b5d3e8798aa
IAmSneak <csudduth04@gmail.com>
2022-05-08 22:08:52 -0400

    Change config type

--- fabric/src/main/java/com/gmail/sneakdevs/diamondeconomyfabric/DiamondEconomyFabric.java
+++ fabric/src/main/java/com/gmail/sneakdevs/diamondeconomyfabric/DiamondEconomyFabric.java
@@ -5,7 +5,7 @@ import com.gmail.sneakdevs.diamondeconomy.DiamondEconomy;
 import com.gmail.sneakdevs.diamondeconomy.config.DiamondEconomyConfig;
 import com.gmail.sneakdevs.diamondeconomy.sql.SQLiteDatabaseManager;
 import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
@@ -19,7 +19,7 @@ public class DiamondEconomyFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        AutoConfig.register(DiamondEconomyConfig.class, Toml4jConfigSerializer::new);
+        AutoConfig.register(DiamondEconomyConfig.class, JanksonConfigSerializer::new);
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> DiamondEconomyCommands.register(dispatcher));
         ServerLifecycleEvents.SERVER_STARTING.register(DiamondEconomyFabric::serverStarting);
     }
--- forge/src/main/java/com/gmail/sneakdevs/diamondeconomyforge/DiamondEconomyForge.java
+++ forge/src/main/java/com/gmail/sneakdevs/diamondeconomyforge/DiamondEconomyForge.java
@@ -5,7 +5,7 @@ import com.gmail.sneakdevs.diamondeconomy.config.DiamondEconomyConfig;
 import com.gmail.sneakdevs.diamondeconomyforge.events.RegisterCommandEventHandler_DiamondEconomy;
 import com.gmail.sneakdevs.diamondeconomyforge.events.StartEventHandler_DiamondEconomy;
 import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
@@ -14,6 +14,6 @@ public class DiamondEconomyForge {
     public DiamondEconomyForge() {
         MinecraftForge.EVENT_BUS.addListener(StartEventHandler_DiamondEconomy::diamondeconomy_onServerStartingEvent);
         MinecraftForge.EVENT_BUS.addListener(RegisterCommandEventHandler_DiamondEconomy::diamondeconomy_registerCommandsEvent);
-        AutoConfig.register(DiamondEconomyConfig.class, Toml4jConfigSerializer::new);
+        AutoConfig.register(DiamondEconomyConfig.class, JanksonConfigSerializer::new);
     }
 }
