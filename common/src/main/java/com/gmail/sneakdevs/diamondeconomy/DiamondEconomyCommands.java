fe092e0366c2ba4a0b316339927a90aa9da6a59c
IAmSneak <csudduth04@gmail.com>
2022-05-08 22:10:34 -0400

    shortcut

--- common/src/main/java/com/gmail/sneakdevs/diamondeconomy/DiamondEconomyCommands.java
+++ common/src/main/java/com/gmail/sneakdevs/diamondeconomy/DiamondEconomyCommands.java
@@ -9,6 +9,8 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.command.argument.ItemStackArgument;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.command.CommandManager;
@@ -23,7 +25,7 @@ public class DiamondEconomyCommands {
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(
-                CommandManager.literal(AutoConfig.getConfigHolder(DiamondEconomyConfig.class).getConfig().commandName)
+                CommandManager.literal(DiamondEconomyConfig.getInstance().commandName)
                         .then(
                                 CommandManager.literal("top")
                                         .then(
@@ -60,20 +62,10 @@ public class DiamondEconomyCommands {
                                                 CommandManager.argument("amount", IntegerArgumentType.integer(1))
                                                         .executes(e -> {
                                                             int amount = IntegerArgumentType.getInteger(e, "amount");
-                                                            return DiamondEconomyCommands.depositCommand(e, amount);
+                                                            return DiamondEconomyCommands.depositCommand(e, amount, null);
                                                         })
                                         )
-                                        .executes(e -> DiamondEconomyCommands.depositCommand(e, 0))
-                        )
-                        .then(
-                                CommandManager.literal("withdraw")
-                                        .then(
-                                                CommandManager.argument("amount", IntegerArgumentType.integer(1))
-                                                        .executes(e -> {
-                                                            int amount = IntegerArgumentType.getInteger(e, "amount");
-                                                            return DiamondEconomyCommands.withdrawCommand(e, amount);
-                                                        })
-                                        )
+                                        .executes(e -> DiamondEconomyCommands.depositCommand(e, 0, null))
                         )
                         .then(
                                 CommandManager.literal("send")
@@ -137,9 +129,9 @@ public class DiamondEconomyCommands {
                         )
         );
 
-        if (AutoConfig.getConfigHolder(DiamondEconomyConfig.class).getConfig().transactionHistory) {
+        if (DiamondEconomyConfig.getInstance().transactionHistory) {
             dispatcher.register(
-                    CommandManager.literal(AutoConfig.getConfigHolder(DiamondEconomyConfig.class).getConfig().commandName)
+                    CommandManager.literal(DiamondEconomyConfig.getInstance().commandName)
                             .then(
                                     CommandManager.literal("admin")
                                             .requires((permission) -> permission.hasPermissionLevel(4))
@@ -158,16 +150,16 @@ public class DiamondEconomyCommands {
             );
         }
 
-        if (AutoConfig.getConfigHolder(DiamondEconomyConfig.class).getConfig().withdrawCommand) {
+        if (DiamondEconomyConfig.getInstance().withdrawCommand) {
             dispatcher.register(
-                    CommandManager.literal(AutoConfig.getConfigHolder(DiamondEconomyConfig.class).getConfig().commandName)
+                    CommandManager.literal(DiamondEconomyConfig.getInstance().commandName)
                             .then(
                                     CommandManager.literal("withdraw")
                                             .then(
                                                     CommandManager.argument("amount", IntegerArgumentType.integer(1))
                                                             .executes(e -> {
                                                                 int amount = IntegerArgumentType.getInteger(e, "amount");
-                                                                return DiamondEconomyCommands.withdrawCommand(e, amount);
+                                                                return DiamondEconomyCommands.withdrawCommand(e, amount, null);
                                                             })
                                             )
                             )
@@ -175,22 +167,25 @@ public class DiamondEconomyCommands {
         }
     }
 
-    public static int withdrawCommand(CommandContext<ServerCommandSource> ctx, int amount) throws CommandSyntaxException {
+    public static int withdrawCommand(CommandContext<ServerCommandSource> ctx, int amount, @Nullable Item item) throws CommandSyntaxException {
         ServerPlayerEntity player = ctx.getSource().getPlayer();
         DatabaseManager dm = DiamondEconomy.getDatabaseManager();
         String uuid = player.getUuidAsString();
 
         if (dm.changeBalance(uuid, -amount)) {
-            DiamondEconomy.dropItem(DiamondEconomyConfig.getCurrency(), amount, player);
-            ctx.getSource().sendFeedback(new LiteralText("Withdrew " + amount + " " + DiamondEconomyConfig.getCurrencyName()), false);
+            //todo
+            DiamondEconomy.dropItem(DiamondEconomyConfig.getCurrency(0), amount, player);
+            //todo
+            ctx.getSource().sendFeedback(new LiteralText("Withdrew " + amount + " " + DiamondEconomyConfig.getCurrencyName(0)), false);
             return 1;
         }
 
-        ctx.getSource().sendFeedback(new LiteralText("You have less than " + amount + " " + DiamondEconomyConfig.getCurrencyName()), false);
+        //todo
+        ctx.getSource().sendFeedback(new LiteralText("You have less than " + amount + " " + DiamondEconomyConfig.getCurrencyName(0)), false);
         return 1;
     }
 
-    public static int depositCommand(CommandContext<ServerCommandSource> ctx, int amount) throws CommandSyntaxException {
+    public static int depositCommand(CommandContext<ServerCommandSource> ctx, int amount, @Nullable Item item) throws CommandSyntaxException {
         ServerPlayerEntity player = ctx.getSource().getPlayer();
         DatabaseManager dm = DiamondEconomy.getDatabaseManager();
         int currencyCount = 0;
@@ -198,7 +193,8 @@ public class DiamondEconomyCommands {
         int newValue = bal + amount;
         if (newValue < Integer.MAX_VALUE && newValue >= 0) {
             for (int i = 0; i < player.getInventory().size(); i++) {
-                if (player.getInventory().getStack(i).getItem().equals(DiamondEconomyConfig.getCurrency())) {
+                //todo
+                if (player.getInventory().getStack(i).getItem().equals(DiamondEconomyConfig.getCurrency(0))) {
                     currencyCount += player.getInventory().getStack(i).getCount();
                     player.getInventory().setStack(i, new ItemStack(Items.AIR));
                 }
@@ -208,22 +204,27 @@ public class DiamondEconomyCommands {
                 newValue = bal + currencyCount;
                 if (newValue < Integer.MAX_VALUE && newValue > 0) {
                     dm.setBalance(player.getUuidAsString(), currencyCount + bal);
-                    ctx.getSource().sendFeedback(new LiteralText("Added " + currencyCount + " " + DiamondEconomyConfig.getCurrencyName() + " to your account"), false);
+                    //todo
+                    ctx.getSource().sendFeedback(new LiteralText("Added " + currencyCount + " " + DiamondEconomyConfig.getCurrencyName(0) + " to your account"), false);
                     return 1;
                 }
-                DiamondEconomy.dropItem(DiamondEconomyConfig.getCurrency(), currencyCount, player);
+                //todo
+                DiamondEconomy.dropItem(DiamondEconomyConfig.getCurrency(0), currencyCount, player);
                 ctx.getSource().sendFeedback(new LiteralText("You do not have enough room in your account"), false);
                 return 1;
             }
 
             if (amount > currencyCount) {
-                DiamondEconomy.dropItem(DiamondEconomyConfig.getCurrency(), currencyCount, player);
-                ctx.getSource().sendFeedback(new LiteralText("You do not have enough " + DiamondEconomyConfig.getCurrencyName() + " in your inventory"), false);
+                //todo
+                DiamondEconomy.dropItem(DiamondEconomyConfig.getCurrency(0), currencyCount, player);
+                //todo
+                ctx.getSource().sendFeedback(new LiteralText("You do not have enough " + DiamondEconomyConfig.getCurrencyName(0) + " in your inventory"), false);
                 return 1;
             }
-            DiamondEconomy.dropItem(DiamondEconomyConfig.getCurrency(), currencyCount - amount, player);
+            //todo
+            DiamondEconomy.dropItem(DiamondEconomyConfig.getCurrency(0), currencyCount - amount, player);
             dm.setBalance(player.getUuidAsString(), amount + bal);
-            ctx.getSource().sendFeedback(new LiteralText("Added " + amount + " " + DiamondEconomyConfig.getCurrencyName() + " to your account"), false);
+            ctx.getSource().sendFeedback(new LiteralText("Added " + amount + " money to your account"), false);
             return 1;
         }
 
@@ -244,19 +245,19 @@ public class DiamondEconomyCommands {
         players.forEach(player -> {
             int bal = dm.getBalanceFromUUID(player.getUuidAsString());
             if (amount > bal) {
-                if (AutoConfig.getConfigHolder(DiamondEconomyConfig.class).getConfig().transactionHistory) {
+                if (DiamondEconomyConfig.getInstance().transactionHistory) {
                     dm.createTransaction("take", finalExecuterUUID, player.getUuidAsString(), bal, bal);
                 }
                 dm.setBalance(player.getUuidAsString(), 0);
             } else {
-                if (AutoConfig.getConfigHolder(DiamondEconomyConfig.class).getConfig().transactionHistory) {
+                if (DiamondEconomyConfig.getInstance().transactionHistory) {
                     dm.createTransaction("take", finalExecuterUUID, player.getUuidAsString(), amount, bal);
                 }
                 dm.setBalance(player.getUuidAsString(), dm.getBalanceFromUUID(player.getUuidAsString()) - amount);
             }
         });
 
-        ctx.getSource().sendFeedback(new LiteralText("Took " + Math.abs(amount) + " " + DiamondEconomyConfig.getCurrencyName() + " from " + players.size() + " players"), true);
+        ctx.getSource().sendFeedback(new LiteralText("Took " + Math.abs(amount) + " money from " + players.size() + " players"), true);
         return players.size();
     }
 
@@ -274,11 +275,11 @@ public class DiamondEconomyCommands {
             int bal = dm.getBalanceFromUUID(player.getUuidAsString());
             int newValue = bal + amount;
             if (newValue < Integer.MAX_VALUE && newValue > 0) {
-                if (AutoConfig.getConfigHolder(DiamondEconomyConfig.class).getConfig().transactionHistory) {
+                if (DiamondEconomyConfig.getInstance().transactionHistory) {
                     dm.createTransaction("give", finalExecuterUUID, player.getUuidAsString(), amount, bal);
                 }
                 dm.setBalance(player.getUuidAsString(), newValue);
-                ctx.getSource().sendFeedback(new LiteralText("Gave " + players.size() + " players " + amount + " " + DiamondEconomyConfig.getCurrencyName()), true);
+                ctx.getSource().sendFeedback(new LiteralText("Gave " + players.size() + " players " + amount + " money"), true);
             } else {
                 ctx.getSource().sendFeedback(new LiteralText("That would go over the max value for " + player.getName().asString()), true);
             }
@@ -297,7 +298,7 @@ public class DiamondEconomyCommands {
 
         String finalExecuterUUID = executerUUID;
         players.forEach(player -> {
-            if (AutoConfig.getConfigHolder(DiamondEconomyConfig.class).getConfig().transactionHistory) {
+            if (DiamondEconomyConfig.getInstance().transactionHistory) {
                 dm.createTransaction("set", finalExecuterUUID, player.getUuidAsString(), amount, dm.getBalanceFromUUID(player.getUuidAsString()));
             }
             dm.setBalance(player.getUuidAsString(), amount);
@@ -312,7 +313,7 @@ public class DiamondEconomyCommands {
         if (player == null) player = ctx.getSource().getPlayer().getName().asString();
         int bal = dm.getBalanceFromName(player);
         if (bal > -1) {
-            ctx.getSource().sendFeedback(new LiteralText(player + " has " + bal + " " + DiamondEconomyConfig.getCurrencyName()), false);
+            ctx.getSource().sendFeedback(new LiteralText(player + " has " + bal + " money"), false);
             return 1;
         }
         ctx.getSource().sendFeedback(new LiteralText("No account was found for player with the name \"" + player + "\""), false);
@@ -342,22 +343,22 @@ public class DiamondEconomyCommands {
         if (!player.getUuidAsString().equals(player1.getUuidAsString())) {
             if (bal >= amount) {
                 if (newValue < Integer.MAX_VALUE && newValue > 0) {
-                    if (AutoConfig.getConfigHolder(DiamondEconomyConfig.class).getConfig().transactionHistory) {
+                    if (DiamondEconomyConfig.getInstance().transactionHistory) {
                         dm.createTransaction("send", player1.getUuidAsString(), player.getUuidAsString(), amount, -1);
                     }
                     dm.setBalance(player.getUuidAsString(), newValue);
                     dm.setBalance(player1.getUuidAsString(), bal - amount);
 
-                    player.sendMessage(new LiteralText("You received " + amount + " " + DiamondEconomyConfig.getCurrencyName() + " from " + player1.getName().asString()), false);
-                    ctx.getSource().sendFeedback(new LiteralText("Sent " + amount + " " + DiamondEconomyConfig.getCurrencyName() + " to " + player.getName().asString()), false);
+                    player.sendMessage(new LiteralText("You received " + amount + " money from " + player1.getName().asString()), false);
+                    ctx.getSource().sendFeedback(new LiteralText("Sent " + amount + " money to " + player.getName().asString()), false);
                 } else {
                     ctx.getSource().sendFeedback(new LiteralText("Failed because that would go over the max value"), false);
                 }
             } else {
-                ctx.getSource().sendFeedback(new LiteralText("You don't have enough " + DiamondEconomyConfig.getCurrencyName()), false);
+                ctx.getSource().sendFeedback(new LiteralText("You don't have enough money"), false);
             }
         } else {
-            ctx.getSource().sendFeedback(new LiteralText("You cant send " + DiamondEconomyConfig.getCurrencyName() + " to yourself"), false);
+            ctx.getSource().sendFeedback(new LiteralText("You cant send money to yourself"), false);
         }
 
         return 1;
