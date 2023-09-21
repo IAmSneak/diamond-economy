f3a88090beb7a5e7c4645c036a66475f8a07e004
IAmSneak <csudduth04@gmail.com>
2023-09-21 13:28:47 -0400

    1.20.2

--- src/main/java/com/gmail/sneakdevs/diamondeconomy/mixin/PlayerListMixin_DiamondEconomy.java
+++ src/main/java/com/gmail/sneakdevs/diamondeconomy/mixin/PlayerListMixin_DiamondEconomy.java
@@ -4,6 +4,7 @@ import com.gmail.sneakdevs.diamondeconomy.DiamondUtils;
 import com.gmail.sneakdevs.diamondeconomy.sql.DatabaseManager;
 import net.minecraft.network.Connection;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.CommonListenerCookie;
 import net.minecraft.server.players.PlayerList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerList.class)
 public class PlayerListMixin_DiamondEconomy {
     @Inject(method = "placeNewPlayer", at = @At("TAIL"))
-    private void diamondeconomy_onPlayerConnectMixin(Connection connection, ServerPlayer serverPlayer, CallbackInfo ci) {
+    private void diamondeconomy_onPlayerConnectMixin(Connection connection, ServerPlayer serverPlayer, CommonListenerCookie commonListenerCookie, CallbackInfo ci) {
         DatabaseManager dm = DiamondUtils.getDatabaseManager();
         String uuid = serverPlayer.getStringUUID();
         String name = serverPlayer.getName().getString();
